6ca996c69859a900ad8e82d65581f466449bc874
cndoit18 <cndoit18@outlook.com>
2021-04-16 13:32:43 +0800

    add list app

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +12,36 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: RandomWord(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWord> {
+  final List<WordPair> _suggestion = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      if (index.isOdd) {
+        return Divider();
+      }
+
+      final int i = index ~/ 2;
+      if (i >= _suggestion.length) {
+        _suggestion.addAll(generateWordPairs().take(10));
+      }
+      return ListTile(
+        title: Text(_suggestion[i].asPascalCase, style: _biggerFont),
+      );
+    });
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RandomWordsState();
 }

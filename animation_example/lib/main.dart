c34ccede103598aa1e3fe0e73651f5d1ce44d1f4
cndoit18 <cndoit18@outlook.com>
2021-04-20 22:17:09 +0800

    add curved

--- animation_example/lib/main.dart
+++ animation_example/lib/main.dart
@@ -19,25 +19,19 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
       ..addStatusListener((status) {
-        switch (status) {
-          case AnimationStatus.dismissed:
-            controller.forward();
-            break;
-          case AnimationStatus.completed:
-            controller.reverse();
-            break;
-          default:
-            break;
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
         }
       });
     controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) =>
-      GrowTransition(child: LogoWidget(), animation: animation);
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
   @override
   void dispose() {
@@ -50,15 +44,20 @@ class AnimatedLogo extends AnimatedWidget {
   AnimatedLogo({Key? key, required Animation<double> animation})
       : super(key: key, listenable: animation);
 
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
     return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: FlutterLogo(),
+        ),
       ),
     );
   }
